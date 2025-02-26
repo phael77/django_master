0c0ec26a75a0c6a73eba5d2480be026d1221aa15
phael77 <rapha555lima@gmail.com>
2025-02-26 18:48:51 -0300

    feat(admin): add admin panel for motorcycles
    - Add admin panel for motorcycles
    - Change the timezone to America/Sao_Paulo
    - Add def __str__ to Motorcycle model

--- motorcycles/admin.py
+++ motorcycles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from motorcycles.models import Motorcycle
 
-# Register your models here.
+
+class MotorcycleAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    search_fields = ('model', 'brand')
+
+admin.site.register(Motorcycle, MotorcycleAdmin)
