7f1159ff9ef472eeed15f24ad99a78392912213a
phael77 <rapha555lima@gmail.com>
2025-02-26 19:12:03 -0300

    feat: Add brand field as a foreign key to Motorcycle model
    - Add brand field as a foreign key to Motorcycle model
    - Add brand field to Motorcycle admin

--- motorcycles/admin.py
+++ motorcycles/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from motorcycles.models import Motorcycle
+from motorcycles.models import Motorcycle, Brand
 
 
 class MotorcycleAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model', 'brand')
 
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
 admin.site.register(Motorcycle, MotorcycleAdmin)
+admin.site.register(Brand, BrandAdmin)
--- motorcycles/models.py
+++ motorcycles/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 class Motorcycle(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
-    brand = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='motorcycle_brand')
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
